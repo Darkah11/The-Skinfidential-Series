603581b0321bdacfb1dd1c93bc461c5308819a6b
egue harris <harrisegue9@gmail.com>
2026-02-16 18:20:48 +0100

    errors fixed2

--- src/components/SearchProducts.tsx
+++ src/components/SearchProducts.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 // import search from "@/public/search.svg";
-import useDebounce from "./useDebounce";
 import { RotatingCircle } from "./Loader";
 import Link from "next/link";
 import { ProductWithId } from "@/types/products";
