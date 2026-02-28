44e6e4256df85a3f3af76939baa9f770f56bdc7f
egue harris <harrisegue9@gmail.com>
2026-02-28 09:27:51 +0100

    attempt 8

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { get } from "@vercel/edge-config";
+// import { get } from "@vercel/edge-config";
 
 type UserRole = "user" | "admin" | null;
 
