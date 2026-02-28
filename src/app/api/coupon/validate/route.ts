1e6829d81d57d798548240edc0e9866ec5a337b4
egue harris <harrisegue9@gmail.com>
2026-02-28 09:11:11 +0100

    attempt 4

--- src/app/api/coupon/validate/route.ts
+++ src/app/api/coupon/validate/route.ts
@@ -1,6 +1,9 @@
 import { validateCoupon } from "@/utils/Validator";
 import { NextRequest, NextResponse } from "next/server";
 
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
