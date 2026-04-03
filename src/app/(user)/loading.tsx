46dbd8e0782dc6168a77f854f70fcf7d574b4f0e
egue harris <harrisegue9@gmail.com>
2026-04-03 11:13:31 +0100

    loader changed

--- src/app/(user)/loading.tsx
+++ src/app/(user)/loading.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import logo from "@/../public/logo.png";
+import Image from "next/image";
 
 export default function loading() {
   return (
-    <div className=' h-screen flex justify-center items-center text-accent'>loading...</div>
-  )
+    <div className=" h-screen flex justify-center items-center text-accent ">
+      <Image src={logo} alt="logo" className=" w-20 h-20 animate-scale-logo" />
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,13 +9,22 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        gold: '#F4E7A3',
-        accent: '#FF3FB9',
+        gold: "#F4E7A3",
+        accent: "#FF3FB9",
         primary: {
-          50: '#916E99',
-          100: '#310246',
+          50: "#916E99",
+          100: "#310246",
         },
       },
+    },
+    keyframes: {
+      "infinite-scale": {
+        "0%, 100%": { transform: "scale(0.7)" },
+        "50%": { transform: "scale(1)" },
+      },
+    },
+    animation: {
+      "scale-logo": "infinite-scale 3s ease-in-out infinite",
     },
   },
   plugins: [],
